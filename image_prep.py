bf04afef1ccad324f62151d8175cd85fd1c6435d
Tugapi <kguoj@outlook.com>
2024-09-04 11:45:42 +0800

    Add canny_from_tensor function

--- image_prep.py
+++ image_prep.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import cv2
+import torch
 
 
 def canny_from_pil(image, low_threshold=100, high_threshold=200):
@@ -10,3 +11,13 @@ def canny_from_pil(image, low_threshold=100, high_threshold=200):
     image = np.concatenate([image, image, image], axis=2)
     control_image = Image.fromarray(image)
     return control_image
+
+def canny_from_tensor(images, low_threshold=100, high_threshold=200):
+    edges_list = []
+    for image in images:
+        image_numpy = image.permute(1, 2, 0).numpy()
+        edge = cv2.Canny(image_numpy.astype(np.uint8), low_threshold, high_threshold)
+        edge = edge[None, :, :]
+        edge = np.concatenate([edge, edge, edge], axis=0)
+        edges_list.append(torch.from_numpy(edge))
+    return torch.stack(edges_list, dim=0)
